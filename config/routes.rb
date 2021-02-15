5fe98516fc532299386e0a405a8cab0d4ceb7822
T-Shikai <tatsuroshikai@gmail.com>
2021-02-15 16:33:12 +0000

    finish cust-login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    registrations: 'customers/registrations'
+  }
   root to: 'homes#top'
   get '/about' => 'homes#about'
 end
