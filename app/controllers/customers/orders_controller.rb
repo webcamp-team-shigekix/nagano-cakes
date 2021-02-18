89ca97e6d6e6530817f09b04789f890cbb0c0cbc
T-Shikai <tatsuroshikai@gmail.com>
2021-02-18 06:44:28 +0000

    after asking

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -6,16 +6,25 @@ class Customers::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-    @ship_cost = 
+    @ship_cost = 800
   end
 
   def create
     @order = Order.new(order_params)
     @order.shipping_cost = 800
-    @cart_products = CartProduct.where(customer_id: current_customer.id)
-    # カート内商品の種類の数だけ@ordered_productを作ってカラムに値入れて全部save
     @order.save
+    # カート内商品の種類の数だけ@ordered_productを作ってカラムに値入れて全部save、その後カート内全削除
+    @cart_products = CartProduct.where(customer_id: current_customer.id)
+    @cart_products.each do |cart_p|
+      @ordered_product = OrderedProduct.new
+      @ordered_product.order_id = @order.id
+      @ordered_product.product_id = cart_p.product_id
+      @ordered_product.count = cart_p.count
+      @ordered_product.tax_included_price = cart_p.product.price*1.1
+      @ordered_product.save
+    end
     @cart_products.destroy_all
+    
     redirect_to orders_finish_path
     
   end
