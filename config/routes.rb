770ff27b5d0582e2dee6bdd9e674d48aaf24b3fb
shun2015 <dwat.pmjg@gmail.com>
2021-02-18 03:03:48 +0000

    resolve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,6 @@ Rails.application.routes.draw do
 
   get '/customers/unsubscribe' => 'customers/customers#unsubscribe'
   patch '/customers/withdraw' => 'customers/customers#withdraw'
-  
   namespace :customers do
     resources :cart_products, only: [:index, :create, :update, :destroy]
   end
@@ -30,7 +29,6 @@ Rails.application.routes.draw do
   namespace :admins do
     resources :products, except: [:destroy]
     resources :product_types, except: [:new, :show, :destroy]
-    resources :customers, only: [:index,:show,:edit,:update]
   end
 
 end
